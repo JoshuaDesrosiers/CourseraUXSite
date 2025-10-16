4852d5da4e5d1c3690eccce2ff9431fe5a611d36
CODEXARS <desrosiersjoshua3@gmail.com>
2025-10-15 23:44:46 -0400

    e

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -1,28 +1,24 @@
-// entry-client.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
 import Wip from "./wip";
-import About from './about'
+import About from "./about";
 import Connect from "./connect";
 
-function MainWrapper() {
-  return (
+hydrateRoot(
+  document.getElementById("root"),
+  <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          
-          <Route index path="wip" element={<Wip />} />
-          <Route index path="about" element={<About />} />
-          <Route index path="connect" element={<Connect />} />
+          <Route path="wip" element={<Wip />} />
+          <Route path="about" element={<About />} />
+          <Route path="connect" element={<Connect />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  );
-}
-
-ReactDOM.hydrateRoot(document.getElementById("root"), <MainWrapper />);
+  </StrictMode>
+);
--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,33 +1,32 @@
 // entry-server.jsx
 import { StrictMode } from "react";
 import { renderToString } from "react-dom/server";
-import { StaticRouter } from "react-router";
+import { StaticRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
 import Wip from "./wip";
-import About from './about'
+import About from "./about";
 import Connect from "./connect";
+
 /**
  * @param {string} _url
  */
-
-
-export function render(_url) {
-  const html = renderToString(
+export async function render(_url) {
+  const appHtml = renderToString(
     <StrictMode>
       <StaticRouter location={_url}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route index path="wip" element={<Wip />} />
-          <Route index path="about" element={<About />} />
-          <Route index path="connect" element={<Connect />} />
+            <Route path="wip" element={<Wip />} />
+            <Route path="about" element={<About />} />
+            <Route path="connect" element={<Connect />} />
           </Route>
         </Routes>
       </StaticRouter>
     </StrictMode>
   );
 
-  return { html };
+  return { appHtml };
 }
